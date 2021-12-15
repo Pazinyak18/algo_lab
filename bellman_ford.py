07dec01bc578ac1f13c6121060a3ab43d6b1c067
Pazyniak Vlad <74894718+Pazinyak18@users.noreply.github.com>
2021-12-15 13:07:22 +0200

    Update bellman_ford.py

--- bellman_ford.py
+++ bellman_ford.py
@@ -1,40 +1,40 @@
 class Graph:
 
     def __init__(self, verticles):
-        self.M = verticles
+        self.verticales = verticles
 
         self.graph = []
 
-    def add_edge(self, a, b, c):
+    def add_edge(self, vertex, connected_vertex, distance):
 
-        self.graph.append([a, b, c])
+        self.graph.append([vertex, connected_vertex, distance])
 
     def print_solution(self, distance):
 
         print("Vertex Distance from Source")
 
-        for k in range(self.M):
-            print("{0}\t\t{1}".format(k, distance[k]))
+        for i in range(self.verticales):
+            print("{0}\t\t{1}".format(i, distance[i]))
 
     def bellman_ford(self, src):
 
-        distance = [float("Inf")] * self.M
+        distances = [float("Inf")] * self.verticales
 
-        distance[src] = 0
+        distances[src] = 0
 
-        for _ in range(self.M - 1):
+        for _ in range(self.verticales - 1):
 
-            for a, b, c in self.graph:
+            for vertex, connected_vertex, distance in self.graph:
 
-                if distance[a] != float("Inf") and distance[a] + c < distance[b]:
-                    distance[b] = distance[a] + c
+                if distances[vertex] != float("Inf") and distances[vertex] + distance < distances[connected_vertex]:
+                    distances[connected_vertex] = distances[vertex] + distance
 
-        for a, b, c in self.graph:
+        for vertex, connected_vertex, distance in self.graph:
 
-            if distance[a] != float("Inf") and distance[a] + c < distance[b]:
+            if distances[vertex] != float("Inf") and distances[vertex] + distance < distances[connected_vertex]:
                 print("Graph contains negative weight cycle")
 
                 return None
 
-        self.print_solution(distance)
-        return distance
+        self.print_solution(distances)
+        return distances
